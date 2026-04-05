491c74305b337a20e0d1c2d000b6c3d260e1d387
uyziu7 <uyziu7@gmail.com>
2026-04-05 09:54:07 +0200

    Initial release of Magasploit v2026.1

---
+++ install.sh
@@ -0,0 +1,23 @@
+#!/data/data/com.termux/files/usr/bin/bash
+
+# Magasploit Auto-Installer
+# Created by uyziu7
+
+echo "[*] Updating system..."
+pkg update -y && pkg upgrade -y
+
+echo "[*] Installing dependencies..."
+pkg install wget dpkg -y
+
+echo "[*] Downloading Magasploit v2026.1..."
+wget https://github.com/uyziu7/mgf/raw/main/mgf_2026.1_all.deb
+
+echo "[*] Installing Magasploit (mgf)..."
+dpkg -i --force-all ./mgf_2026.1_all.deb
+
+echo "[*] Cleaning up..."
+rm mgf_2026.1_all.deb
+
+echo -e "\n[+] Magasploit installed successfully!"
+echo "[+] Type 'mgfconsole' to start."
+
